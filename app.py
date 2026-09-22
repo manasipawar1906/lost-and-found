0d30a8923d624721b2b389a3cf23257cddf5c084
manthan <manthanmane784@gmail.com>
2026-09-22 08:12:03 -0700

    Updated frontend styling and database connection

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def get_db():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Manasi@2024",  
+        password="",  
         database="lost_and_found"
     )
 
